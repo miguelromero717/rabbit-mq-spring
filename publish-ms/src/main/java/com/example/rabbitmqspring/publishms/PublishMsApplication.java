44327f57744c6681074e71338c1ae539a70e8d1b
Miguel Angel Romero <miguelromero717@gmail.com>
2021-10-28 21:28:24 -0500

    Create publish-ms module

---
+++ publish-ms/src/main/java/com/example/rabbitmqspring/publishms/PublishMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqspring.publishms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublishMsApplication.class, args);
+    }
+
+}
