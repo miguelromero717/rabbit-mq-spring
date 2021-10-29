9d8a4aa971671072841ee4714a8ae50290f0ab8e
Miguel Angel Romero <miguelromero717@gmail.com>
2021-10-29 18:34:33 -0500

    Implement publish message microservice
    - Implement endpoint to receive the text to send to queue
    - Implement queue service to send message to rabbit
    - Config all docker services

--- publish-ms/src/main/java/com/example/rabbitmqspring/publishms/PublishMsApplication.java
+++ publish-ms/src/main/java/com/example/rabbitmqspring/publishms/PublishMsApplication.java
@@ -1,9 +1,14 @@
 package com.example.rabbitmqspring.publishms;
 
+import com.example.rabbitmqspring.publishms.config.QueueProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        QueueProperties.class
+})
 public class PublishMsApplication {
 
     public static void main(String[] args) {
